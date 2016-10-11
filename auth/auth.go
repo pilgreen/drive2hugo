bd03e97bf76aff095407619c7da91612fdb20f59
Jay Pilgreen <jpilgreen@kcstar.com>
2016-10-11 15:46:12 -0500

    adding Go files

---
+++ auth/auth.go
@@ -0,0 +1,110 @@
+package auth
+
+import (
+  "encoding/json"
+  "fmt"
+  "io/ioutil"
+  "log"
+  "net/http"
+  "net/url"
+  "os"
+  "os/user"
+  "path/filepath"
+  
+  "golang.org/x/net/context"
+  "golang.org/x/oauth2"
+  "golang.org/x/oauth2/google"
+  "google.golang.org/api/drive/v3"
+)
+
+// GetService uses a path to load a Google App credential file
+// downloaded from the Google developer console. It returns a Service.
+func GetService(path string) (*drive.Service, error) {
+  ctx := context.Background()
+  
+  // Check that the secret file is there
+  b, err := ioutil.ReadFile(path)
+  if err != nil {
+    log.Fatalf("Unable to read client secret file: %v", err)
+  }
+
+  // Load a previous token file
+  driveConfig, err := google.ConfigFromJSON(b, drive.DriveReadonlyScope)
+  if err != nil {
+    log.Fatalf("Unable to parse client secret file to config: %v", err)
+  }
+
+  // Set up the client and server
+  client := GetClient(ctx, driveConfig)
+  return drive.New(client)
+}
+
+// GetClient uses a Context and Config to retrieve a Token
+// then generate a Client. It returns the generated Client.
+func GetClient(ctx context.Context, config *oauth2.Config) *http.Client {
+  cacheFile, err := tokenCacheFile()
+  if err != nil {
+    log.Fatalf("Unable to get path to cached credential file. %v", err)
+  }
+  tok, err := tokenFromFile(cacheFile)
+  if err != nil {
+    tok = getTokenFromWeb(config)
+    saveToken(cacheFile, tok)
+  }
+  return config.Client(ctx, tok)
+}
+
+// getTokenFromWeb uses Config to request a Token.
+// It returns the retrieved Token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+  authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+  fmt.Printf("Go to the following link in your browser then type the authorization code: \n%v\n", authURL)
+
+  var code string
+  if _, err := fmt.Scan(&code); err != nil {
+    log.Fatalf("Unable to read authorization code %v", err)
+  }
+
+  tok, err := config.Exchange(oauth2.NoContext, code)
+  if err != nil {
+    log.Fatalf("Unable to retrieve token from web %v", err)
+  }
+  return tok
+}
+
+// tokenCacheFile generates credential file path/filename.
+// It returns the generated credential path/filename.
+func tokenCacheFile() (string, error) {
+  usr, err := user.Current()
+  if err != nil {
+    return "", err
+  }
+  tokenCacheDir := filepath.Join(usr.HomeDir, ".drivecontent")
+  os.MkdirAll(tokenCacheDir, 0700)
+  return filepath.Join(tokenCacheDir, url.QueryEscape("auth-token.json")), err
+}
+
+// tokenFromFile retrieves a Token from a given file path.
+// It returns the retrieved Token and any read error encountered.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+  f, err := os.Open(file)
+  if err != nil {
+    return nil, err
+  }
+  t := &oauth2.Token{}
+  err = json.NewDecoder(f).Decode(t)
+  defer f.Close()
+  return t, err
+}
+
+// saveToken uses a file path to create a file and store the
+// token in it.
+func saveToken(file string, token *oauth2.Token) {
+  fmt.Printf("Saving credential file to: %s\n", file)
+  f, err := os.Create(file)
+  if err != nil {
+    log.Fatalf("Unable to cache oauth token: %v", err)
+  }
+  defer f.Close()
+  json.NewEncoder(f).Encode(token)
+}
---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+  "encoding/json"
+  "flag"
+  "fmt"
+  "io/ioutil"
+  "log"
+  "os"
+  "path/filepath"
+  "time"
+
+  "github.com/pilgreen/drivecontent/auth"
+  "google.golang.org/api/drive/v3"
+)
+
+type Config struct {
+  Folders []Folder
+}
+
+type Folder struct {
+  Id string
+  Path string
+}
+
+func loadConfig(file string) (*Config, error) {
+  f, err := os.Open(file)
+  if err != nil {
+    return nil, err
+  }
+  c := &Config{}
+  err = json.NewDecoder(f).Decode(c)
+  defer f.Close()
+  return c, err
+}
+
+func modTime() (string, error) {
+  b, err := ioutil.ReadFile("hd.modified.txt")
+  if err != nil {
+    return "", err
+  }
+
+  return string(b), err
+}
+
+func saveModFile() {
+  f, err := os.Create("hd.modified.txt")
+  if err != nil {
+    log.Fatalf("Unable to create modified file: %v\n", err)
+  }
+
+  defer f.Close()
+  t, _ := time.Now().MarshalText()
+  f.Write(t)
+}
+
+func main() {
+  sPtr := flag.String("secret", "hd.secret.json", "File downloaded from Google Console")
+  cPtr := flag.String("config", "hd.config.json", "Path to the config file")
+  mPtr := flag.Bool("modified", false, "Only pulls files modified since the last run")
+  flag.Parse()
+
+  // Get the Service
+  srv, err := auth.GetService(*sPtr)
+  if err != nil {
+    log.Fatalf("Unable to get the Drive Service: %v", err)
+  }
+
+  // Load the config
+  config, err := loadConfig(*cPtr);
+  if err != nil {
+    log.Fatalf("Unable to read a config file: %v", err)
+  }
+
+  // Main loop
+  for _, folder := range config.Folders {
+    var q string
+
+    mod, err := modTime()
+    if *mPtr == true && err == nil {
+      q = fmt.Sprintf("mimeType = 'application/vnd.google-apps.document' and '%s' in parents and modifiedTime > '%s'", folder.Id, mod)
+    } else {
+      q = fmt.Sprintf("mimeType = 'application/vnd.google-apps.document' and '%s' in parents", folder.Id)
+    }
+
+    r, err := srv.Files.List().Q(q).Fields("files(name, id)").Do()
+    if err != nil {
+      log.Fatalf("Unable to retrieve files: %v", err)
+    }
+
+    log.Printf("%d files found in %s\n", len(r.Files), folder.Id)
+    if len(r.Files) > 0 {
+      os.MkdirAll(folder.Path, 0755)
+      queue := make(chan *drive.File, len(r.Files))
+      done := make(chan bool)
+
+      // Set up the goroutine to handle the individual files
+      go func() {
+        for {
+          file, more := <-queue
+          if more {
+            req, err := srv.Files.Export(file.Id, "text/plain").Download()
+            if err != nil {
+              log.Fatalf("Unable to download file: %v", err)
+            }
+
+            filename := file.Id + ".md"
+            path := filepath.Join(folder.Path, filename)
+            body, _ := ioutil.ReadAll(req.Body)
+
+            err = ioutil.WriteFile(path, body, 0644)
+            if err != nil {
+              log.Printf("Problem writing %s: %v\n", file.Name, err)
+            } else {
+              log.Printf("%s written as %s\n", file.Name, path)
+            }
+          } else {
+            log.Printf("all done with folder: %s\n", folder.Id)
+            done <- true
+            return
+          }
+        }
+      }()
+
+
+      for _, file := range r.Files {
+        queue <- file
+      }
+
+      close(queue)
+      <- done
+    }
+  }
+
+  // Save the modified file for the next run
+  if *mPtr == true {
+    saveModFile()
+  }
+}
